21170a66b0806da82257b2cbe7c1d54e9c61b987
jlbuild <juliabuildbot@gmail.com>
2021-12-16 19:07:17 +0000

    MPICH_jll build 3.4.3+0

--- src/wrappers/armv6l-linux-gnueabihf-libgfortran3.jl
+++ src/wrappers/armv6l-linux-gnueabihf-libgfortran3.jl
@@ -3,19 +3,14 @@ export generate_compile_time_mpi_constants, libload_time_mpi_constants, libmpi, 
 
 using CompilerSupportLibraries_jll
 JLLWrappers.@generate_wrapper_header("MPICH")
-JLLWrappers.@declare_executable_product(generate_compile_time_mpi_constants)
 JLLWrappers.@declare_library_product(libload_time_mpi_constants, "libload_time_mpi_constants.so")
 JLLWrappers.@declare_library_product(libmpi, "libmpi.so.12")
 JLLWrappers.@declare_library_product(libmpicxx, "libmpicxx.so.12")
 JLLWrappers.@declare_library_product(libmpifort, "libmpifort.so.12")
+JLLWrappers.@declare_executable_product(generate_compile_time_mpi_constants)
 JLLWrappers.@declare_executable_product(mpiexec)
 function __init__()
     JLLWrappers.@generate_init_header(CompilerSupportLibraries_jll)
-    JLLWrappers.@init_executable_product(
-        generate_compile_time_mpi_constants,
-        "bin/generate_compile_time_mpi_constants",
-    )
-
     JLLWrappers.@init_library_product(
         libload_time_mpi_constants,
         "lib/libload_time_mpi_constants.so",
@@ -38,6 +33,11 @@ function __init__()
         libmpifort,
         "lib/libmpifort.so",
         RTLD_LAZY | RTLD_DEEPBIND,
+    )
+
+    JLLWrappers.@init_executable_product(
+        generate_compile_time_mpi_constants,
+        "bin/generate_compile_time_mpi_constants",
     )
 
     JLLWrappers.@init_executable_product(
