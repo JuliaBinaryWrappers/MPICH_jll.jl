486f928f787a6d706f746be9a4baf1ca638fa217
jlbuild <juliabuildbot@gmail.com>
2026-02-06 16:55:10 +0000

    MPICH_jll build 5.0.0+0

--- src/MPICH_jll.jl
+++ src/MPICH_jll.jl
@@ -2,11 +2,10 @@
 baremodule MPICH_jll
 using Base
 using Base: UUID
-using LazyArtifacts
 using MPIPreferences
 Base.include(@__MODULE__, joinpath("..", ".pkg", "platform_augmentation.jl"))
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("MPICH")
-JLLWrappers.@generate_main_file("MPICH", UUID("7cb0a576-ebde-5e09-9194-50597f1243b4"))
+JLLWrappers.@generate_main_file("MPICH", Base.UUID("7cb0a576-ebde-5e09-9194-50597f1243b4"))
 end  # module MPICH_jll
